256a92a4f7376da76075acc4ea7b1fd00ef46e67
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-11-01 22:09:15 -0300

    Add React Navigation

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import Cart from './pages/Cart';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      Cart,
+    },
+    {
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#141419',
+        },
+        headerTintColor: '#fff',
+      },
+    }
+  )
+);
+
+export default Routes;
